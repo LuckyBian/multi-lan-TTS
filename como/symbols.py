8aeb40a529faf5ea27808f70f7f71f078585d08d
LuckyBian <2658603082@qq.com>
2024-08-20 14:49:57 +0800

    como

---
+++ como/symbols.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('/home/weizhenbian/vits_all')
+
+from como import cmudict
+
+_pad        = '_'
+_punctuation = '!\'(),.:;? '
+_special = '-'
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+# Prepend "@" to ARPAbet symbols to ensure uniqueness:
+_arpabet = ['@' + s for s in cmudict.valid_symbols]
+
+# Export all symbols:
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+
+print(len(_letters))
+print(len(_arpabet))
